2129581a50cc29ffbae26b35d4089a48af8234e4
aditigour <aditigour06@gmail.com>
2024-02-19 20:31:51 +0530

    added user builder

--- src/main/java/com/rio/messenger/builder/UserBuilder.java
+++ src/main/java/com/rio/messenger/builder/UserBuilder.java
@@ -1,2 +1,9 @@
-package com.rio.messenger.builder;public class UserBuilder {
+package com.rio.messenger.builder;
+
+import com.rio.messenger.entity.User;
+
+public final class UserBuilder {
+  public static User buildUser(String userName, String passcode){
+    return User.builder().userName(userName).password(passcode).build();
+  }
 }
