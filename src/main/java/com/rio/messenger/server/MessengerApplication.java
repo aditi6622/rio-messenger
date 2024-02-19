b0edb23237e09b2e929988776d8b0370a6df91cb
aditigour <aditigour06@gmail.com>
2024-02-19 20:33:05 +0530

    modified server class and deleted redundant IDE files

--- src/main/java/com/rio/messenger/server/MessengerApplication.java
+++ src/main/java/com/rio/messenger/server/MessengerApplication.java
@@ -1,9 +1,17 @@
-package com.rio.messenger;
+package com.rio.messenger.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.rio.messenger"})
+@EnableJpaRepositories(basePackages = {"com.rio.messenger"})
+@EntityScan(basePackages = {"com.rio.messenger"})
+@EnableJpaAuditing
 public class MessengerApplication {
 
 	public static void main(String[] args) {
