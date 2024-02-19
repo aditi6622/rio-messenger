8b44c99ff79d48ff58dd7cd55c38826decad6bb6
aditigour <aditigour06@gmail.com>
2024-02-19 20:31:38 +0530

    added repo

--- src/main/java/com/rio/messenger/repository/UserRepository.java
+++ src/main/java/com/rio/messenger/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.rio.messenger.repository;public class UserRepository {
+package com.rio.messenger.repository;
+
+import com.rio.messenger.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+  User findByUserName(String userName);
 }
